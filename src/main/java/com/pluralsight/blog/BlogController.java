c6c68c48318f9a582edb867fd59c1ecfb1a22144
Nicolasf740 <82244505+Nicolasf740@users.noreply.github.com>
2021-04-30 16:57:53 -0300

    Blog Commit 001

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,6 +4,7 @@ import com.pluralsight.blog.data.PostRepository;
 import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,6 +22,13 @@ public BlogController(PostRepository postRepository) {
     List<Post> posts = postRepository.getAllPosts();
 modelMap.put("posts", posts);
         return "home";
+    }
+
+    @RequestMapping("/post/{id}")
+    public String postDetails(@PathVariable long id, ModelMap modelMap) {
+    Post post = postRepository.findById(id);
+    modelMap.put("post", post);
+    return "post-details";
     }
 
 }
