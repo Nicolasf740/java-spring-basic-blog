ea9737f49262853f9374755eddbd27022330688a
Nicolasf740 <82244505+Nicolasf740@users.noreply.github.com>
2021-04-30 17:08:24 -0300

    Blog Commit 001

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -25,7 +25,7 @@ modelMap.put("posts", posts);
     }
 
     @RequestMapping("/post/{id}")
-    public String postDetails(@PathVariable long id, ModelMap modelMap) {
+    public String postDetails(@PathVariable Long id, ModelMap modelMap) {
     Post post = postRepository.findById(id);
     modelMap.put("post", post);
     return "post-details";
